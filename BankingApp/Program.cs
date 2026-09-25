0b63f49cd18fe5a18a5cdd37870ab03af4aae844
c-H097 <meriemh706@gmail.com>
2026-09-25 13:59:23 +0300

    Updated code

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -24,39 +24,68 @@ void BuildBankingApp()
         if (choice == 1)
         {
             Console.WriteLine("You have chosen to deposit or withdraw funds\n" +
-                "How much do you want to deposit?\n" +
-                "Enter amount including pence using a decimal point");
-            
-            double depositAmount = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"Your original account balance was {accountBalance}");
+                "Which options do you want to choose?\n" +
+                "1 - Deposit\n" +
+                "2 - Withdraw");
+            int choice2 = Convert.ToInt32(Console.ReadLine());
 
-            //account balance = account balance + deposit amount
-            accountBalance += depositAmount;
-            Console.WriteLine($"Your new account balance is {accountBalance}");
-            
-        }
+            if (choice2 == 1)
+            {
+                // deposit funds
+                Console.WriteLine("How much do you want to deposit?\n" +
+                    "Enter amount including pence using a decimal point");
 
-        else if (choice == 2)
-        {
-            // view current account info
-            Console.WriteLine("You have chose to view current account info\n" +
-                $"Your current account balance is {accountBalance}");
+                double depositAmount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Your original account balance was {accountBalance}");
+
+                //account balance = account balance + deposit amount
+                accountBalance += depositAmount;
+                Console.WriteLine($"Your new account balance is {accountBalance}");
+            }
+
+            else if (choice2 == 2)
+            {
+                // withdraw funds
+                Console.WriteLine("How much do you want to withdraw?\n" +
+                    "Enter amount including pence using a decimal point");
+
+                double withdrawAmount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Your original balance was {accountBalance}");
+
+                //account balance = account balance - withdraw amount
+                if (withdrawAmount > accountBalance)
+                {
+                    Console.WriteLine("Insufficient funds. Withdrawal amount exceeds account balance.");
+                }
+                else
+                {
+                    accountBalance -= withdrawAmount;
+                    Console.WriteLine($"Your new account balance is {accountBalance}");
+                }
+
+            }
+
+            else if (choice == 2)
+            {
+                // view current account info
+                Console.WriteLine("You have chose to view current account info\n" +
+                    $"Your current account balance is {accountBalance}");
+            }
+            else
+            {
+                // view current account info
+                Console.WriteLine("You have chosen to view current account info");
+                accountBalance = 1000.0;
+                Console.WriteLine($"Your current account balance is {accountBalance}");
+            }
+
         }
         else
         {
-            // view current account info
-            Console.WriteLine("You have chosen to view current account info");
-            accountBalance = 1000.0;
-            Console.WriteLine($"Your current account balance is {accountBalance}");
+            // display error message
+            Console.WriteLine("Invalid choice. Please enter 1 or 2.");
         }
 
     }
-    else
-    {
-        // display error message
-        Console.WriteLine("Invalid choice. Please enter 1 or 2.");
-    }
-
 }
-
 BuildBankingApp();
